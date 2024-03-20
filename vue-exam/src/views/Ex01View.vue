d4d148fe5546de1d13193ce64b2352a8834fd588
rhs0402 <rhs0402@naver.com>
2024-03-20 16:39:17 +0900

    4

---
+++ vue-exam/src/views/Ex01View.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>vue공부</h1>
+    <h2>{{ subTitle }}</h2>
+    <p>{{ content }}</p>
+</template>
+<script>
+
+export default {
+    name: "Ex01View",
+    components: {},
+    data() {
+        return {
+            subTitle: "바인딩된 소제목",
+            content: "vue 공부시작"
+        };
+    },
+    methods: {},
+};
+</script>
+<style></style>
---
+++ vue-exam/src/views/Ex02View.vue
@@ -0,0 +1,59 @@
+<template>
+    <h1>{{ title }}</h1>
+    <h2>{{ subTitle }}</h2>
+
+    <ul>
+        <li>{{ menus[0] }}</li>
+        <li>{{ menus[1] }}</li>
+        <li>{{ menus[2] }}</li>
+    </ul>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>번호</th>
+                <th>이름</th>
+                <th>핸드폰</th>
+                <th>회사</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>{{ personVo.no }}</td>
+                <td>{{ personVo.name }}</td>
+                <td>{{ personVo.hp }}</td>
+                <td>{{ personVo.company }}</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <p>{{ content }}</p>
+    <p v-html="content"></p>
+
+    <img v-bind:src="imgUrl" alt="">
+
+</template>
+<script>
+
+export default {
+    name: "Ex02View",
+    components: {},
+    data() {
+        return {
+            title: "vue공부",
+            subTitle: "데이터바인딩",
+            menus: ["게시판", "갤러리", "방명록"],
+            personVo:{
+                no: 1,
+                name: "이효리",
+                hp: "010-9999-9999",
+                company: "02-9999-9999"
+            },
+            content: "지금부터 <strong>열심히</strong> 공부하겠습니다.",
+            imgUrl: "https://mml.pstatic.net/www/mobile/edit/20240318_1095/upload_1710771613913KFcqp.gif"
+        };
+    },
+    methods: {},
+};
+</script>
+<style></style>

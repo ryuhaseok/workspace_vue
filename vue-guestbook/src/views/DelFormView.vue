7c86f8a21094ea00cbaeb92b83f461ce6b030ea5
rhs0402 <rhs0402@naver.com>
2024-03-22 17:51:54 +0900

    123

--- vue-guestbook/src/views/DelFormView.vue
+++ vue-guestbook/src/views/DelFormView.vue
@@ -41,7 +41,14 @@ export default {
                     responseType: 'json' //수신타입
                 }).then(response => {
                     console.log(response.data); //수신데이타
-                    this.$router.push({ path: '/' });
+
+                    if(response.data>0){
+                        this.$router.push({ path: '/' });
+
+                    }else{
+                        alert("패스워드를 확인해주세요");
+                    }
+
 
                 }).catch(error => {
                     console.log(error);
